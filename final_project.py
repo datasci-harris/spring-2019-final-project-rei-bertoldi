f43a8bfcae5fe91b707dfe2fd8073bfc40314912
rhbertoldi <49281577+rhbertoldi@users.noreply.github.com>
2019-05-21 17:08:03 -0500

    Update final_project.py
    loop is not working, its overwritting and only writing a csv for 2016

--- final_project.py
+++ final_project.py
@@ -23,11 +23,14 @@ def get_rows(response):
         td_tags = row.find_all('td')
         unparsed_rows.append([val.text.replace(',','') for val in td_tags])
     unparsed_rows = unparsed_rows[1:-1]
+    return unparsed_rows    
 
 def row_parser(row):
     return ','.join(row)
 
 parsed_rows = [row_parser(row) for row in unparsed_rows]
+
+pd.DataFrame(np.array(parsed_rows
 
 header = 'year, company, eia_id, cec_id, plant, state, capacity_MW, gross_MWhm, net_MWh'
 parsed_rows.insert(0, header)
@@ -40,16 +43,28 @@ path = r'C:\Users\User02\Desktop\Harris\Programming\test.csv'
 with open(path, 'w') as ofile:
     ofile.write(document)
     
-#write this damn function
+#path = r'C:\Users\User02\Desktop\Harris\Programming\final_project'
 
+#my_ofile = r'C:\Users\User02\Desktop\Harris\Programming\final_project\{}.csv'
 
-#looping the csvs in a loop
+#for year in ['2017', '2016']:
+#    response = requests.post(url, data={'newYear':year})
+#    unparsed_rows = get_rows(response)
+#    parsed_rows = [row_parser(row) for row in unparsed_rows]
+#    header = 'year, company, eia_id, cec_id, plant, state, capacity_MW, gross_MWhm, net_MWh'
+#    parsed_rows.insert(0, header)
+#    document = '\n'.join(parsed_rows)
 
-my_ofile = 'path\{}.csv'
+my_ofile = r'C:\Users\User02\Desktop\Harris\Programming\final_project\{}.csv'
 
-for year in ['2018', '2017', '2016]:
-    response = requests.post(url, data={'newYear':year})
-#    soup = BeautifulSoup(response.text, 'html.parser')
-    parsed_data = my_parsing_function(response)
-    with open(my_ofile, 'w') as ofile:
-        ofile.write(parsed_data)
+with open(my_ofile, 'w') as ofile:
+    headers = 'year, company, eia_id, cec_id, plant, state, capacity_MW, gross_MWhm, net_MWh\n'
+    ofile.write(headers)
+    for year in ['2018','2017', '2016']:
+        response = requests.post(url, data={'newYear':year})
+        unparsed_rows = get_rows(response)
+        parsed_rows = [row_parser(row) for row in unparsed_rows]
+        document = '\n'.join(parsed_rows)
+    ofile.write(document)
+        
+    
