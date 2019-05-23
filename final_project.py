3c67c5b0d6963a2ad0f3972f144b9056b7db04a8
rhbertoldi <49281577+rhbertoldi@users.noreply.github.com>
2019-05-22 22:32:03 -0500

    beginning data exploration
    playing around with some groupby functions

--- final_project.py
+++ final_project.py
@@ -31,20 +31,56 @@ def row_parser(row):
     return ','.join(row)
 
 #creating a loop to export the csvs for all the years
-my_ofile = r'C:\Users\User02\Desktop\Harris\Programming\final_project\{}.csv'
+my_ofile = r'C:\Users\User02\Documents\GitHub\spring-2019-final-project-rei-bertoldi\{}.csv'
 
 for year in range(2003, 2019):
     year = str(year)
     response = requests.post(url, data={'newYear':year})
     unparsed_rows = get_rows(response)
     parsed_rows = [row_parser(row) for row in unparsed_rows]
-    header = 'year, company, eia_id, cec_id, plant, state, capacity_MW, gross_MWhm, net_MWh'
+    header = 'year,company,eia_id,cec_id,plant,state,capacity_MW,gross_MWhm,net_MWh'
     parsed_rows.insert(0, header)
     document = '\n'.join(parsed_rows)
     ofile_yr = my_ofile.format(year)
     with open(ofile_yr, 'w') as ofile:
         ofile.write(document)
+
+#importing and merging data
+import os
+import pandas as pd
+base_path = r'C:\Users\User02\Documents\GitHub\spring-2019-final-project-rei-bertoldi'
+
+dfs = []
+
+for file in os.listdir(path):
+    if not file.endswith('.csv'):
+        continue
+    df = pd.read_csv(os.path.join(base_path, file))
+    dfs.append(df)
     
+wind_energy = pd.concat(dfs, axis=0, ignore_index=True)
+len(wind_energy.index.values)
+#net energy over time
+net_energy = wind_energy.groupby('year', as_index=False).sum()
+#plot
+import matplotlib.pyplot as plt
+
+#looking at the contributions of each company 
+unique_values = wind_energy.groupby(['year', 'company']).size().reset_index().rename(columns={0:'unique'})
+#big four:
+    #foundation wind power
+    #terra-gen operating company
+    #FPL energy
+    #seawest 
+    
+company_energy = wind_energy.groupby(['company','year'], as_index=False)['net_MWh'].sum()
+filtered_energy = company_energy[company_energy["company"].isin([""]), :]
+
+
+
+
+
+#
     
 
     
